e2c326f13170e06dc6c19eea8a3893cd8bd21014
Sabrina Akbar <sabrinaakbar141@gmail.com>
2019-06-01 19:56:36 -0500

    added int main()

--- ImageProcessing_1/main.cpp
+++ ImageProcessing_1/main.cpp
@@ -1,3 +1,15 @@
-#include<iostream>
-#include<string>
-#include<opencv2/core/core.hpp>
+#include <iostream>
+#include <string>
+#include <opencv2/core/core.hpp>
+
+
+using namespace std;
+
+
+int main() {
+
+
+
+
+	return 0;
+}
